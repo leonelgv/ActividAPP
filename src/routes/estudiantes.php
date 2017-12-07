3314e66ef799c2e21b4d4e3e94fd219e5b36a8ba
Leonel Gonzalez Vidales <leonelgv@outlook.com>
2017-12-06 18:59:46 -0600

    feat: agregar un nuevo estudiante

--- src/routes/estudiantes.php
+++ src/routes/estudiantes.php
@@ -25,7 +25,7 @@ $app->get('/api/estudiantes', function(Request $request, Response $response){
     }
 });
 
-// Get Single Customer
+// Obtener un estudiante por no de control
 $app->get('/api/estudiantes/{nocontrol}', function(Request $request, Response $response){
     $nocontrol = $request->getAttribute('nocontrol');
 
@@ -41,6 +41,41 @@ $app->get('/api/estudiantes/{nocontrol}', function(Request $request, Response $r
         $estudiante = $stmt->fetchAll(PDO::FETCH_OBJ);
         $db = null;
         echo json_encode($estudiante);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Agregar un estudiante
+$app->post('/api/estudiantes/add', function(Request $request, Response $response){
+    $nocontrol = $request->getParam('nocontrol');
+    $nombre = $request->getParam('nombre');
+    $apellidop = $request->getParam('apellidop');
+    $apellidom = $request->getParam('apellidom');
+    $semestre = $request->getParam('semestre');
+    $carrera_clave = $request->getParam('carrera_clave');
+
+    $sql = "INSERT INTO estudiante (nocontrol, nombre_estudiante, apellido_p_estudiante, apellido_m_estudiante, semestre, carrera_clave) VALUES (:nocontrol, :nombre, :apellidop, :apellidom, :semestre, :carrera_clave)";
+
+    try{
+        // Obtener el objeto DB
+        $db = new db();
+        // Conectar
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':nocontrol',      $nocontrol);
+        $stmt->bindParam(':nombre',         $nombre);
+        $stmt->bindParam(':apellidop',      $apellidop);
+        $stmt->bindParam(':apellidom',      $apellidom);
+        $stmt->bindParam(':semestre',       $semestre);
+        $stmt->bindParam(':carrera_clave',  $carrera_clave);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Estudiante agregado"}';
+
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
